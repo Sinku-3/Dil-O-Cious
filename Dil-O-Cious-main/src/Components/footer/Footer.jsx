cd89bbf870355dd7735ec2fdaf66005d5d42b2a6
Sinku-3 <22BCS16467@cuchd.in>
2025-12-14 22:14:22 +0530

    Update Footer.jsx

--- Dil-O-Cious-main/src/Components/footer/Footer.jsx
+++ Dil-O-Cious-main/src/Components/footer/Footer.jsx
@@ -35,15 +35,15 @@ const Footer = () => {
                 <div id='footerDetails'>
                     <div className="contact">
                         <h1>Contact</h1>
-                        <p><i className="fa-solid fa-square-phone"></i> <span>+91-8840337523</span></p>
-                        <p><i className="fa-solid fa-envelope"></i> <span>sr1071995@gmail.com</span></p>
+                        <p><i className="fa-solid fa-square-phone"></i> <span>+91-6378284813</span></p>
+                        <p><i className="fa-solid fa-envelope"></i> <span>shinkukumawat75369@gmail.com</span></p>
                         <p>Live customer care service available Mon-Sat 10 am - 7 pm on non-public holidays</p>
                     </div>
                     <div className="details">
                         Dilocious is your one-stop-shop if you seek and crave for local and regional flavours, unique products and authentically experience different cultures of entire India. At Dilocious, you can get limited available speciality food, a huge variety of tastes and offerings, regional variants of famous food and enjoy far distant seasonal delicacies right your doorstep.
                     </div>
                 </div>
-                <div id='copyright'> &copy; Shashank Rai </div>
+                <div id='copyright'> &copy; sinku kumawat </div>
                 <Tippy content={'Back to top'} placement='left'>
                     <i className='fa-solid fa-circle-chevron-up' id='backToTop' style={{ display: visible ? 'inline' : 'none' }} onClick={scrollToTop}  ></i>
                 </Tippy>
